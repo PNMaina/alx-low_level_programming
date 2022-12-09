fe2bd92c81914cd2ef54c104d73d643414fe01f5
PNMaina <pmaina057@gmail.com>
2022-12-09 07:12:15 -0800

    size is not granduer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,14 +2,14 @@
 /**
  * main -print out size of data types in c
  * code by pnmaina ict
- * return: 0
+ * Return: 0
 */
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
+	char a;
+	int b;
+	long int c;
+	long long int d;
 	float f;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
@@ -18,3 +18,4 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
+}
