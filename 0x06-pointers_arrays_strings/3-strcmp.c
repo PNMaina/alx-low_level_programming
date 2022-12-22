e8104b22fbc24e1420e9ab4a59403dc09f58cca9
PNMaina <pmaina057@gmail.com>
2022-12-22 04:33:35 -0800

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _strcmp - compares two strings values
- * @s1: string 1
- * @s2: string 2
- * Return: the difference in s1 and s2
+ * -strcmp - compares two strings values
+ *  @s1: string 1
+ *  @s2: string 2
+ *  Return: the difference in s1 and s2
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] != '\0' && s2[1] != '\0')
 	{
-		if (sl[i] != s2[i])
+		if (s1[i] != s2[i])
 		{
-			return (s1[i] - s2[i]);
+			return(s1[i] - s2[i]);
 		}
 	i++;
 	}
